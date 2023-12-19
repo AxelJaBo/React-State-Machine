c8d9695690e02c72ec90a2885ef74d055b060ab9
AxelJaBo <axel.910921@gmail.com>
2023-12-18 20:49:14 -0800

    Estados

--- src/Components/BaseLayout.jsx
+++ src/Components/BaseLayout.jsx
@@ -5,6 +5,9 @@ import { bookingMachine } from "../Machines/bookingMachine";
 const BaseLayaout = () => {
     const [state, send] = useMachine(bookingMachine);
     console.log('nuestra maquina', state);
+    console.log('matches true', state.matches('initial'));
+    console.log('matches false', state.matches('tickets'));
+    console.log('can', state.can('FINISH'));
     return (
         <div>
             Hola
